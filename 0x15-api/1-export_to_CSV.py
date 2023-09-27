a94ffdc5c961698ab4f1df35eb102ebe2183e864
Katekani Madingana <madinganakn@gmail.com>
2023-09-27 13:50:12 -0700

    task 1 api

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+    this module accesses an api and retrieves the data
+"""
+
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    user_response = requests.get("{}/users/{}".format(base_url, employee_id))
+    user_data = user_response.json()
+
+    if "id" not in user_data:
+        sys.exit(1)
+
+    todos_response = requests.get("{}/todos?userId={}".format(base_url,
+                                  employee_id))
+    todos_data = todos_response.json()
+    print(todos_data)
+    csv_filename = "{}.csv".format(user_data["id"])
+    with open(csv_filename, mode='w', newline="") as csv_file:
+        fieldnames = ["USER_ID",
+                      "USERNAME",
+                      "TASK_COMPLETED_STATUS",
+                      "TASK_TITLE"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for task in todos_data:
+            writer.writerow({
+                "USER_ID": user_data["id"],
+                "USERNAME": user_data["username"],
+                "TASK_COMPLETED_STATUS": task["completed"],
+                "TASK_TITLE": task["title"]
+                })
