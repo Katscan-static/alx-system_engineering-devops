1b15d159715d648643c53ea61e75a6d97025baa9
Katekani Madingana <madinganakn@gmail.com>
2023-10-10 10:27:37 -0700

    get the top ten posts for a subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    This module returns the the top 10 postd of a  reddit
+"""
+
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+        This function gets the top 10 posts
+
+        args:
+            subreddit (str): the reddit to get the top 10 posts from
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "My Reddit Scrapper"}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+
+            for post in data["data"]["children"]:
+                title = post["data"]["title"]
+                print(title)
+
+        except KeyError:
+                print(None)
+    else:
+        print(None)
